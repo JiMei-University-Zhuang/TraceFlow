b7b819c58d90d0d912b6aff74f7c2814d63ab8b6
JiMei-University-Zhuang <202241054088@jmu.edu.cn>
2025-03-06 11:26:50 +0800

    测试: 测试代码格式化钩子

--- packages/trace-server/src/test-format.ts
+++ packages/trace-server/src/test-format.ts
@@ -1,7 +1,4 @@
-function   testFormat(   param1:string,param2   :  number)    {
-    console.log(  "test"   )
-        return {
-      test:true,
-        format:   "wrong"
-    }
+export function testFormat2(param1: string, param2: number): string {
+  console.log('test2', param1, param2);
+  return `${param1}-${param2}`;
 }
