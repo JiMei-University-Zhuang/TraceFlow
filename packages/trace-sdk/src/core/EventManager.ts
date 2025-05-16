7bd7e6dbc58b64065c953c3385124938f80798b0
JiMei-University-Zhuang <202241054088@jmu.edu.cn>
2025-05-16 20:29:08 +0800

    fix: 🐛 移除旧架构

--- packages/trace-sdk/src/core/EventManager.ts
+++
@@ -1,9 +0,0 @@
-export const createBaseEvent = (eventType: string, eventData?: Record<string, any>) => {
-  return {
-    eventType,
-    ...eventData,
-    timestamp: Date.now(),
-    pageUrl: window.location.href,
-    attempts: 0,
-  };
-};
